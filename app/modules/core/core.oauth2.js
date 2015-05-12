067f9ee11ad30ba4091ea7c6d698cab5c8e9e820
Artur Stępień <AStepien@ydp.pl>
2015-05-12 12:48:41 +0200

    OAuth2 run.

--- app/modules/core/core.oauth2.js
+++ app/modules/core/core.oauth2.js
@@ -1,28 +1,30 @@
-//(function () {
-//    'use strict';
-//
-//    angular.module('app')
-//        .config(['OAuthProvider', function (OAuthProvider) {
-//            OAuthProvider.configure({
-//                baseUrl: 'https://api.website.com',
-//                clientId: 'CLIENT_ID',
-//                clientSecret: 'CLIENT_SECRET' // optional
-//            });
-//        }])
-//        .run(['$rootScope', '$window', function ($rootScope, $window, OAuth) {
-//            $rootScope.$on('oauth:error', function (event, rejection) {
-//                // Ignore `invalid_grant` error - should be catched on `LoginController`.
-//                if ('invalid_grant' === rejection.data.error) {
-//                    return;
-//                }
-//
-//                // Refresh token when a `invalid_token` error occurs.
-//                if ('invalid_token' === rejection.data.error) {
-//                    return OAuth.getRefreshToken();
-//                }
-//
-//                // Redirect to `/login` with the `error_reason`.
-//                return $window.location.href = '/login?error_reason=' + rejection.data.error;
-//            });
-//        }]);
-//})();
+(function () {
+    'use strict';
+
+    angular.module('app')
+        .run(['$rootScope', '$injector', function ($rootScope, $injector) {
+            $injector.get("$http").defaults.transformRequest = function (data, headersGetter) {
+                var sessionService = new sessionService();
+
+                if (sessionService.isLogged()) {
+                    headersGetter()['Authorization'] = "Bearer " + sessionService.getAccessToken();
+                }
+                if (data) {
+                    return angular.toJson(data);
+                }
+
+                ////////////
+                function sessionService() {
+                    return {
+                        isLogged: isLogged
+                    };
+
+                    function isLogged() {
+                        return 100;
+                    }
+                }
+            };
+        }]);
+
+
+})();
