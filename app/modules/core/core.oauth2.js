6a345d892f1b6973102ac82409c02161139c01cc
Artur Stępień <AStepien@ydp.pl>
2015-05-13 10:36:30 +0200

    OAuth2 run delete.

--- app/modules/core/core.oauth2.js
+++ app/modules/core/core.oauth2.js
@@ -1,27 +1,29 @@
-(function () {
-    'use strict';
+//Not in use
 
-    angular.module('app')
-        .factory('sessionService', sessionService)
-        .run(['$rootScope', '$injector', 'sessionService', function ($rootScope, $injector, sessionService) {
-            $injector.get("$http").defaults.transformRequest = function (data, headersGetter) {
-                if (sessionService.getAccessToken()) {
-                    headersGetter()['Authorization'] = "Bearer " + sessionService.getAccessToken();
-                }
-            };
-        }]);
-
-    ////////////
-    function sessionService() {
-        var _token = localStorage.getItem('access_token');
-
-        return {
-            getAccessToken: getAccessToken
-        };
-
-        function getAccessToken() {
-            return _token;
-        }
-    }
-
-})();
+//(function () {
+//    'use strict';
+//
+//    angular.module('app')
+//        .factory('sessionService', sessionService)
+//        .run(['$rootScope', '$injector', 'sessionService', function ($rootScope, $injector, sessionService) {
+//            $injector.get("$http").defaults.transformRequest = function (data, headersGetter) {
+//                if (sessionService.getAccessToken()) {
+//                    headersGetter()['Authorization'] = "Bearer " + sessionService.getAccessToken();
+//                }
+//            };
+//        }]);
+//
+//    ////////////
+//    function sessionService() {
+//        var _token = localStorage.getItem('access_token');
+//
+//        return {
+//            getAccessToken: getAccessToken
+//        };
+//
+//        function getAccessToken() {
+//            return _token;
+//        }
+//    }
+//
+//})();
