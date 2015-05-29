33d67615ec5c44cdfe3c42d543f69a74d53d13bc
Artur Stępień <AStepien@ydp.pl>
2015-05-29 16:01:25 +0200

    Minor.

--- app/modules/scorm/scorm.js
+++ app/modules/scorm/scorm.js
@@ -61,9 +61,9 @@
 
         vm.url = {
             scorm: {
-                klasa_1: basePath + "/app/modules/scorm/tpl/scorm_1_3_klasa1/lpc_pl-test_lpc_klasa1/proxy/scorm13/script_00001.emt.html",
-                klasa_2: basePath + "/app/modules/scorm/tpl/scorm_1_3_klasa2/lpc_pl-test_lpc_klasa2/proxy/scorm13/script_00002.emt.html",
-                klasa_3: basePath + "/app/modules/scorm/tpl/scorm_1_3_klasa3/lpc_pl-test_lpc_klasa3/proxy/scorm13/script_00003.emt.html"
+                klasa_1: basePath + "/app/modules/scorm/tpl/klasa1/proxy/scorm13/script_00001.emt.html",
+                klasa_2: basePath + "/app/modules/scorm/tpl/klasa2/proxy/scorm13/script_00002.emt.html",
+                klasa_3: basePath + "/app/modules/scorm/tpl/klasa3/proxy/scorm13/script_00003.emt.html"
             }
         };
 
@@ -91,7 +91,6 @@
                     $rootScope.logout();
 
                     $state.go('login');
-                    logger.success('Dane zostały wysłane pomyślnie!');
                 }, function () {
                     logger.error('Dane nie zostały wysłane');
                     vm.isDisabled = false;
--- app/modules/scorm/tpl/lower_primary_curriculum_demo_tech/content/1873.cs
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8" standalone="no"?><toc xmlns="http://www.ydp.pl/leo/toc" xmlns:xlink="http://www.w3.org/1999/xlink" title="Toc (819.cs)">
-   <section id="s0">
-      <player version="3.3"/>
-      <section id="b3540" kind="" name="demo">
-         <description/>
-         <item id="a24031" name="Selection of Multimedia and Activity Screens" xlink:href="script_00001.emt.xml"/>
-      </section>
-   </section>
-</toc>
